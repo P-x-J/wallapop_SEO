d410c56293b00682db9e4e1fdb6e67bb4a159021
P-x-J <magoelectrico631@gmail.com>
2026-06-26 13:02:21 +0200

    FIxed some bugs

--- main.py
+++ main.py
@@ -2,11 +2,25 @@ import sys
 import time
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 driver = webdriver.Chrome()
+driver.get("https://wallapop.com/app/catalog/published")
 
-driver.get("https://google.com")
+time.sleep(4)
 
-time.sleep(10)
+wait = WebDriverWait(driver, 10)
+
+# click "Reject all"
+reject_btn = wait.until(
+    EC.element_to_be_clickable((By.XPATH, "//button[contains(.,'Reject all')]"))
+)
+reject_btn.click()
+
+
+time.sleep(40)
 
 driver.quit()
