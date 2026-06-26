fa2eb9eba2be3e1c75a5d208857433a12c68c9ca
P-x-J <magoelectrico631@gmail.com>
2026-06-26 12:29:18 +0200

    Made working webdriver from chromium

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ import sys
 import time
 
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
 
-service = Service(executable_path="/geckodriver-v0.37.0-linux-aarch64/geckodriver")
-driver = webdriver.Chrome(service=service)
-
-driver = webdriver.Firefox(service=service)
+driver = webdriver.Chrome()
 
 driver.get("https://google.com")
 
