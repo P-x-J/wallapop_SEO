211df3b51e886a6e022f1e6b2230aaf6cdaba5de
P-x-J <magoelectrico631@gmail.com>
2026-06-26 11:49:25 +0200

    Created main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path="")
+driver = webdriver.Chrome(service=service)
