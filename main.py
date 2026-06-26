043c5154f391b41d1e8b3cb8f2170a340f669336
P-x-J <magoelectrico631@gmail.com>
2026-06-26 12:04:35 +0200

    Added firefox webdriver

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import sys
+import time
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service
 
-service = Service(executable_path="")
+service = Service(executable_path="/geckodriver-v0.37.0-linux-aarch64/geckodriver")
 driver = webdriver.Chrome(service=service)
+
+driver = webdriver.Firefox(service=service)
+
+driver.get("https://google.com")
+
+time.sleep(10)
+
+driver.quit()
