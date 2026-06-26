ea05585429a813cb241cb540f64b72f24b99c466
P-x-J <magoelectrico631@gmail.com>
2026-06-26 19:08:43 +0200

    Fixed item_list function

--- items_list_input.py
+++ items_list_input.py
@@ -8,16 +8,13 @@ def prompt_items_list():
     
     items_links_list = []
 
-    working = True
+
     while True:
-        item_link = str(input("Insert link: "))
-        if item_link=="Stop":
-            working = False
+        item_link = str(input("Insert link: ").strip())
+        if item_link.lower()=="stop":
+            break
         else:
             items_links_list.append(item_link.strip())
 
     return items_links_list
-
-        
-print(prompt_items_list())
     
