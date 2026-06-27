b8eb4e8a2e3ce1169d3a06dd54281c2402437821
P-x-J <magoelectrico631@gmail.com>
2026-06-27 17:08:31 +0200

    Fixed prompt time function and main

--- prompt_time_intervals.py
+++ prompt_time_intervals.py
@@ -1,7 +1,12 @@
 def time_interval_verification ():
     while True:
         try:
-            time_interval = int(input("How much time do you want to wait between intervals? (minutes)"))
+            time_interval = int(input("How much time do you want to wait between intervals? (minutes) "))
+            if time_interval <= 0:
+                print("Please enter a positive number.")
+                continue
             break
         except ValueError:
             print("Invalid input. Please enter a valid integer.")
+
+    return time_interval
