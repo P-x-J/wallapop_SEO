39a5c0ee0c5727425907e7a1b65857fda39e3e01
P-x-J <magoelectrico631@gmail.com>
2026-06-27 09:50:10 +0200

    Added while loop

--- main.py
+++ main.py
@@ -19,40 +19,38 @@ wait = WebDriverWait(driver, 10)
 login()
 items_list_link = prompt_items_list()
 
-for item_link in items_list_link:
-    driver.get(item_link)
-    time.sleep(10)
+while True:
+    for item_link in items_list_link:
+        driver.get(item_link)
+        time.sleep(10)
 
-    # Enter editor mode
-    edit_button = driver.find_element(By.CLASS_NAME, "walla-button__button walla-button__button--full-width walla-button__button--medium walla-button__button--primary")
-    edit_button.click()
-    time.sleep(7)
+        # Enter editor mode
+        edit_button = driver.find_element(By.CLASS_NAME, "walla-button__button walla-button__button--full-width walla-button__button--medium walla-button__button--primary")
+        edit_button.click()
+        time.sleep(7)
 
-    # Added a dot to the description
-    description = wait.until(EC.presence_of_element_located((By.ID, "description")))
-    append_dot(driver, description)
+        # Added a dot to the description
+        description = wait.until(EC.presence_of_element_located((By.ID, "description")))
+        append_dot(driver, description)
 
-    refresh_button = driver.find_element(By.CLASS_NAME, "walla-button__button walla-button__button--full-width walla-button__button--medium walla-button__button--primary")
-    time.sleep(5)
-
-
-for item_link in items_list_link:
-    driver.get(item_link)
-    time.sleep(10)
-
-    # Enter editor mode
-    edit_button = driver.find_element(By.CLASS_NAME, "walla-button__button walla-button__button--full-width walla-button__button--medium walla-button__button--primary")
-    edit_button.click()
-    time.sleep(7)
-
-    # Added a dot to the description
-    description = wait.until(EC.presence_of_element_located((By.ID, "description")))
-    append_dot(driver, description)
-    refresh_button = driver.find_element(By.CLASS_NAME, "walla-button__button walla-button__button--full-width walla-button__button--medium walla-button__button--primary")
-    time.sleep(5)
+        refresh_button = driver.find_element(By.CLASS_NAME, "walla-button__button walla-button__button--full-width walla-button__button--medium walla-button__button--primary")
+        time.sleep(5)
 
 
+    for item_link in items_list_link:
+        driver.get(item_link)
+        time.sleep(10)
 
+        # Enter editor mode
+        edit_button = driver.find_element(By.CLASS_NAME, "walla-button__button walla-button__button--full-width walla-button__button--medium walla-button__button--primary")
+        edit_button.click()
+        time.sleep(7)
+
+        # Added a dot to the description
+        description = wait.until(EC.presence_of_element_located((By.ID, "description")))
+        append_dot(driver, description)
+        refresh_button = driver.find_element(By.CLASS_NAME, "walla-button__button walla-button__button--full-width walla-button__button--medium walla-button__button--primary")
+        time.sleep(5)
 
 
 time.sleep(10)
