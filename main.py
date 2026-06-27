b9ab41941a0d24555d314c42e061b1881f78bcf3
P-x-J <magoelectrico631@gmail.com>
2026-06-27 16:52:35 +0200

    New version of main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 import time
-import pyautogui
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,52 +10,23 @@ from user_login_input import login
 from items_list_input import prompt_items_list
 from add_remove_dot_utils import append_dot
 from add_remove_dot_utils import remove_trailing_dot
+from adding_dot_loop import add_dot_loop
+from removing_dot_loop import removing_dot_loop
+from prompt_time_intervals import time_interval_verification
 
 driver = webdriver.Chrome()
-driver.get("https://wallapop.com/app/catalog/published")
+driver.get("https://es.wallapop.com/app/catalog/published")
 
 wait = WebDriverWait(driver, 10)
-#Let user login to the catalog
+# Let user login to the catalog
 login()
 items_list_link = prompt_items_list()
+time_interval = time_interval_verification()
 
 while True:
-    for item_link in items_list_link:
-        driver.get(item_link)
-        time.sleep(10)
-
-        # Enter editor mode
-        edit_button = wait.until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/main/div/div[2]/div/aside/section/div[3]/div/div/walla-button[1]"))
-        )
-        driver.execute_script("arguments[0].scrollIntoView(true);", edit_button)
-        edit_button.click()
-        time.sleep(7)
-
-        # Added a dot to the description
-        description = wait.until(EC.presence_of_element_located((By.ID, "description")))
-        append_dot(driver, description)
-
-        refresh_button = driver.find_element(By.CSS_SELECTOR, "walla-button__button walla-button__button--full-width walla-button__button--medium walla-button__button--primary")
-        time.sleep(5)
-
-
-    for item_link in items_list_link:
-        driver.get(item_link)
-        time.sleep(10)
-
-        # Enter editor mode
-        edit_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@role='button' and .//span[contains(text(), 'Editar')]]")))
-        edit_button.click()
-        time.sleep(7)
-
-        # Added a dot to the description
-        description = wait.until(EC.presence_of_element_located((By.ID, "description")))
-        append_dot(driver, description)
-        refresh_button = driver.find_element(By.CLASS_NAME, "walla-button__button walla-button__button--full-width walla-button__button--medium walla-button__button--primary")
-        time.sleep(5)
-
-
-time.sleep(10)
-
-driver.quit()
+    print("STARTING ADDING DOT LOOPS")
+    add_dot_loop(items_list_link, wait, driver)
+    print("START REMOVING DOT LOOPS")
+    removing_dot_loop(items_list_link, wait, driver)
+    print(f"Waiting {time_interval} minutes")
+    time.sleep(int(time_interval*60))
